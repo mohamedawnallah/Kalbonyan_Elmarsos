a51c79dc1d1e430ffb80dce21e4f307402406344
Mohamed Awnallah <69568555+mohamedmohey2352@users.noreply.github.com>
2021-10-14 07:30:56 +0200

    Handlling Bugs

--- Modern Javascript Bootcamp Course Colt Steele/Movie Fight/index.js
+++ Modern Javascript Bootcamp Course Colt Steele/Movie Fight/index.js
@@ -9,14 +9,25 @@ const fetchData = async (searchTerm) => {
 		.catch((error) => {
 			console.log(error);
 		});
-	console.log(response.data);
+        if (response.data.Error) {
+            return [] ;
+        }
+	return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-
-const onInput = (event) => {
-	fetchData(event.target.value);
+const onInput = async (event) => {
+	const movies = await fetchData(event.target.value);
+    
+	for (let movie of movies) {
+		const div = document.createElement('div');
+		div.innerHTML = `
+                <img src= ${movie.Poster}/>
+                <h1>${movie.Title}</h1>
+            `;
+		document.querySelector('#target').appendChild(div);
+	}
 };
 
 input.addEventListener('input', debounce(onInput, 500));
