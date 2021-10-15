23c604826ecf17d71c7800ac9d29cc3e999b8b15
Mohamed Awnallah <69568555+mohamedmohey2352@users.noreply.github.com>
2021-10-15 02:28:38 +0200

    High Reusable Autocomplete Function

--- Modern Javascript Bootcamp Course Colt Steele/Movie Fight/index.js
+++ Modern Javascript Bootcamp Course Colt Steele/Movie Fight/index.js
@@ -1,65 +1,34 @@
-const fetchData = async (searchTerm) => {
-	const response = await axios
-		.get('http://www.omdbapi.com/', {
-			params: {
-				apikey: 'd4bcc0fd',
-				s: searchTerm
-			}
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-	if (response.data.Error) {
-		return [];
-	}
-	return response.data.Search;
-};
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-const input = document.querySelector('input');
-const dropDown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-	if (!movies.length) {
-		dropDown.classList.remove('is-active');
-		return;
-	}
-	dropDown.classList.add('is-active');
-	resultsWrapper.innerHTML = '';
-
-	for (let movie of movies) {
-		const option = document.createElement('a');
+createAutoComplete({
+	root: document.querySelector('.autocomplete'),
+	renderOption(movie) {
 		const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
-                <img src= ${imgSrc}/>
-                ${movie.Title}
+		return `
+                <img src= '${imgSrc}'/>
+                ${movie.Title} (${movie.Year})
             `;
-		option.addEventListener('click', () => {
-			dropDown.classList.remove('is-active');
-			input.value = movie.Title;
-			onMovieSelect(movie);
-		});
-		resultsWrapper.appendChild(option);
-	}
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-document.addEventListener('click', (event) => {
-	if (!root.contains(event.target)) {
-		dropDown.classList.remove('is-active');
+	},
+	async onOptionSelect(movie) {
+		await onMovieSelect(movie);
+	},
+	inputValue(movie) {
+		return movie.Title;
+	},
+	async fetchData(searchTerm) {
+		const response = await axios
+			.get('http://www.omdbapi.com/', {
+				params: {
+					apikey: 'd4bcc0fd',
+					s: searchTerm
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+		if (response.data.Error) {
+			return [];
+		}
+		return response.data.Search;
 	}
 });
 
@@ -74,11 +43,11 @@ const onMovieSelect = async (movie) => {
 		.catch((error) => {
 			console.log(error);
 		});
-        document.querySelector('#summary').innerHTML = movieTemplate(response.data);
-    };
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
 
-const movieTemplate = movieDetails => {
-   return `
+const movieTemplate = (movieDetails) => {
+	return `
     <article class="media">
         <figure class="media-left">
             <p class="image">
@@ -93,5 +62,30 @@ const movieTemplate = movieDetails => {
             </div>
         </div>
     </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetails.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    
+    <article class="notification is-primary">
+      <p class="title">${movieDetails.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieDetails.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieDetails.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p> 
+    </article>
+
+    <article class="notification is-primary">
+      <p class="title">${movieDetails.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  
    `;
-}
+};
