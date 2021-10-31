5480fe196084f2fce7f4521a74557962e9712a22
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-10-31 19:00:55 +0200

    Updating Records and Adding Filtering Logic

--- Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/repositories/users.js
+++ Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/repositories/users.js
@@ -41,19 +41,52 @@ class UsersRepository {
 
 	async getOne(id) {
 		const records = await this.getAll();
-		return records.find(record => record.id === id);
+		return records.find((record) => record.id === id);
 	}
 
 	async delete(id) {
 		const records = await this.getAll();
-		const filteredRecords = records.filter(record => record.id !== id);
+		const filteredRecords = records.filter((record) => record.id !== id);
 		await this.writeAll(filteredRecords);
+	}
+
+	async update(id, attrs) {
+		const records = await this.getAll();
+		const record = records.find((record) => record.id === id);
+		if (!record) {
+			throw new Error(`Record with ${id} not found`);
+		}
+		// record === {email : "test@test.com"}
+		// attrs === { password: 'mypassword}
+		Object.assign(record, attrs);
+		// record === {email: "test@test.com", password:"mypassword"}
+		await this.writeAll(records);
+	}
+
+	async getOneBy(filters) {
+		const records = await this.getAll();
+
+		for (let record of records) {
+			let found = true;
+
+			for (let key in filters) {
+				if (record[key] !== filters[key]) {
+					found = false;
+				}
+			}
+			if (found) {
+				return record;
+			}
+		}
 	}
 }
 
 const test = async () => {
 	const repo = new UsersRepository('users.json');
-	await repo.delete('c793530a');
+	await repo.create({ email: 'test@test.com' });
+	await repo.update('a0c92fde', { password: 'myPassword' });
+	const user = await repo.getOneBy({ password: 'myPsassword' });
+	console.log(user)
 };
 
 test();
