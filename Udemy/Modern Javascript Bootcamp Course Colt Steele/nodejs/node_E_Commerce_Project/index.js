82f13242513495c88a21fd08058bd540ac93edad
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-11-03 15:12:03 +0200

    Fixing Bug Email Validation

--- Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/index.js
+++ Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-
 const authRouter = require('./routes/admin/auth');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-	cookieSession({
-		keys: [ 'lfjdalksfkelkawr' ]
-	})
+  cookieSession({
+    keys: ['lkasld235j']
+  })
 );
-
 app.use(authRouter);
 
 app.listen(3000, () => {
-	console.log('Listening');
+  console.log('Listening');
 });
