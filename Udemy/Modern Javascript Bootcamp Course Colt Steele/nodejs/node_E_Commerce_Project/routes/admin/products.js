6a80514f983bc7f5d56a35e4705ed01f96281756
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-11-05 08:03:50 +0200

    Edit product Funcationality + Support with url params and extract them

--- Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/routes/admin/products.js
+++ Udemy/Modern Javascript Bootcamp Course Colt Steele/nodejs/node_E_Commerce_Project/routes/admin/products.js
@@ -4,30 +4,65 @@ const multer = require('multer');
 const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
-const productsIndexTemplate = require('../../views/admin/products/index')
+const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
+
 const { requireTitle, requirePrice } = require('./validators');
 
 const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
-router.get('/admin/products',requireAuth , async(req, res) => {
-	
- const products = await productsRepo.getAll();
+router.get('/admin/products', requireAuth, async (req, res) => {
+	const products = await productsRepo.getAll();
 
- res.send(productsIndexTemplate({products}))
+	res.send(productsIndexTemplate({ products }));
 });
 
-router.get('/admin/products/new',requireAuth , (req, res) => {
-
+router.get('/admin/products/new', requireAuth, (req, res) => {
 	res.send(productsNewTemplate({}));
-  
 });
 
-router.post('/admin/products/new', requireAuth, upload.single('image'), [ requireTitle, requirePrice ], handleErrors(productsNewTemplate), async (req, res) => {
-	const image = req.file.buffer.toString('base64');
-	const { title, price } = req.body;
-	await productsRepo.create({ title, price, image });
+router.post(
+	'/admin/products/new',
+	requireAuth,
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsNewTemplate),
+	async (req, res) => {
+		const image = req.file.buffer.toString('base64');
+		const { title, price } = req.body;
+		await productsRepo.create({ title, price, image });
 
-	res.redirect('/admin/products')
+		res.redirect('/admin/products');
+	}
+);
+
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+	const product = await productsRepo.getOne(req.params.id);
+
+	if (!product) {
+		res.send('Product Not Found');
+	}
+	res.send(productsEditTemplate({ product }));
 });
+
+router.post(
+	'/admin/products/:id/edit',
+	requireAuth,
+	upload.single('image'),
+	[ requireTitle, requirePrice ],
+	handleErrors(productsEditTemplate),
+	async (req, res) => {
+		const changes = req.body;
+		if (req.file) {
+			changes.image = req.file.buffer.toString('base64');
+		}
+		try {
+			await productsRepo.update(req.params.id, changes);
+		} catch (err) {
+			return res.send('Could not find the item');
+		}
+		res.redirect('/admin/products');
+	}
+);
 module.exports = router;
