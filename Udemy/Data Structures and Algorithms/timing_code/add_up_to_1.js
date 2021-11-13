33a08f0a63ad7f0ab95325785c8bc00e0cca497c
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-11-13 07:12:17 +0200

    Timing Code through Timing Functions Pros and Cons

---
+++ Udemy/Data Structures and Algorithms/timing_code/add_up_to_1.js
@@ -0,0 +1,14 @@
+//slower than second solution
+const {performance} = require('perf_hooks');
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 1; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
---
+++ Udemy/Data Structures and Algorithms/timing_code/add_up_to_2.js
@@ -0,0 +1,10 @@
+//Faster Than First Solution
+const {performance} = require('perf_hooks');
+function addUpTo(n) {
+    return n * (n + 1) / 2;
+}
+
+let t1 = performance.now();
+addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
