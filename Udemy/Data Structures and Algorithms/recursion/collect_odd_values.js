7b249f9c4761b718406e715a22013e881d5426f0
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2021-11-18 10:00:07 +0200

    Improve Performance of Collect Odd Value Space Complexity from O(N) to O(1)

--- Udemy/Data Structures and Algorithms/recursion/collect_odd_values.js
+++ Udemy/Data Structures and Algorithms/recursion/collect_odd_values.js
@@ -1,6 +1,9 @@
 
 // Helper Method Recursion Design Pattern
 // Bottom Recursion  Type
+
+//With Space Complexity : O(N)
+
 function collectOddValues(arr) {
    let oddResults = []
 
@@ -19,3 +22,27 @@ function collectOddValues(arr) {
 }
 
 console.log(collectOddValues([10, 13, 15, 20, 22]));
+
+
+// With Space Complexity : O(1)
+
+function collectOddValues(arr) {
+    let oddResults = {}
+ 
+    function helper(helperInput) {
+        let firstVal = helperInput[0];
+        if (helperInput.length === 0) {
+            return;
+        }
+        if (firstVal % 2 !==0) {
+
+            oddResults[firstVal] = firstVal;
+        }
+        helper(helperInput.slice(1))
+    }
+    helper(arr);
+ 
+    return oddResults;
+ }
+ 
+ console.log(collectOddValues([10, 13, 15, 20, 22]));
